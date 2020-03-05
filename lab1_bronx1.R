06c40a793699f9cf470b1237be6658781994f872
Scout Oatman-Gaitan <scout3303@gmail.com>
2020-03-05 15:42:25 -0500

    add bronx 1

---
+++ lab1_bronx1.R
@@ -0,0 +1,41 @@
+library(gdata)
+library(readxl)
+#faster xls reader but requires perl!
+bronx1<-read.csv("rollingsales_bronx.csv", header=T, skip=4) 
+bronx1<-bronx1[which(bronx1$GROSS.SQUARE.FEET!="0" & bronx1$LAND.SQUARE.FEET!="0" & bronx1$SALE.PRICE!="$0"),]
+
+#alternate
+#library("xlsx", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+#bronx1<-read.xlsx("<SOMEWHERE>/rollingsales_bronx.xls",pattern="BOROUGH",stringsAsFactors=FALSE,sheetIndex=1,startRow=5,header=TRUE)
+View(bronx1)
+#
+attach(bronx1) # If you choose to attach, leave out the "data=." in lm regression
+SALE.PRICE<-sub("\\$","",SALE.PRICE) 
+SALE.PRICE<-as.numeric(gsub(",","", SALE.PRICE)) 
+GROSS.SQUARE.FEET<-as.numeric(gsub(",","", GROSS.SQUARE.FEET)) 
+LAND.SQUARE.FEET<-as.numeric(gsub(",","", LAND.SQUARE.FEET)) 
+plot(log(GROSS.SQUARE.FEET), log(SALE.PRICE)) 
+m1<-lm(log(SALE.PRICE)~log(GROSS.SQUARE.FEET))
+summary(m1)
+abline(m1,col="red",lwd=2)
+plot(resid(m1))
+
+# Model 2
+m2<-lm(log(SALE.PRICE)~log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD))
+summary(m2)
+plot(resid(m2))
+# Suppress intercept - using "0+ ..."
+m2a<-lm(log(SALE.PRICE)~0+log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD))
+summary(m2a)
+plot(resid(m2a))
+
+# Model 3
+m3<-lm(log(SALE.PRICE)~0+log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD)+factor(BUILDING.CLASS.CATEGORY))
+summary(m3)
+plot(resid(m3))
+
+# Model 4
+m4<-lm(log(SALE.PRICE)~0+log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD)*factor(BUILDING.CLASS.CATEGORY))
+summary(m4)
+plot(resid(m4))
+#
