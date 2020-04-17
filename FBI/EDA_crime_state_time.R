a764923bb14b7632a5897b9f2e23508f4376d820
Scout Oatman-Gaitan <54646126+scoutog@users.noreply.github.com>
2020-04-16 20:36:10 -0400

    update

--- FBI/EDA_crime_state_time.R
+++ FBI/EDA_crime_state_time.R
@@ -50,11 +50,10 @@ plot_usmap(data = rates2010, values = "Population", include =  c(.south_atlantic
 ########################################
 ########################################
 summary(rates_df$Year)
-View(total_df)
 
-x <- "2016violent.jpg"
-y <- 2016
-z <- "Violent Crime in 2016"
+x <- "2014violent.jpg"
+y <- 2014
+z <- "Violent Crime in 2014"
 
 #jpeg(x, width = 568, height = 376)
 plot_usmap(data = subset(total_df, Year == y), values = "Violent.crime.total",  color = orange, labels=FALSE) + 
@@ -66,15 +65,34 @@ plot_usmap(data = subset(total_df, Year == y), values = "Violent.crime.total",  
   labs(title = z, caption = "Source: FBI UCR")
 #dev.off() 
 
+###############################################################
+###############################################################
+###############################################################
+
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(plotly)
+library(RColorBrewer)
+library(choroplethrMaps)
+library(choroplethr)
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
+
+by_state <- total_df  %>% filter(Year == 2000) %>% group_by(state) %>% select(state, Year, Violent.crime.total) 
+
+head(by_state, 10)
+tail(by_state, 10)
+
+ggplot(head(by_state, 10), aes(reorder(state, -Violent.crime.total), Violent.crime.total, fill = Violent.crime.total)) + 
+  geom_bar(stat = "identity") + xlab("State") + ylab("Number of Complaints") + 
+  theme(axis.text.x = element_text(angle = 50, size = 10, vjust = 0.4, face = "bold"), 
+        plot.title = element_text(size = 20, face = "bold", vjust = 2), 
+        axis.title.x = element_text(face = "bold", size = 15, vjust = -0.35), 
+        axis.title.y = element_text(face = "bold", vjust = 0.35, size = 15)) + 
+  theme(legend.position = "none" )
 
 
 
-
-
-plot_usmap(data = subset(total_df, Year == y), values = "Violent.crime.total",  color = orange, labels=FALSE) + 
-  scale_fill_continuous( low = "white", high = orange, 
-                         name = "Violent Crime", label = scales::comma
-  ) + 
-  theme(legend.position = "right") + 
-  theme(panel.background = element_rect(colour = "black")) + 
-  labs(title = z, caption = "Source: FBI UCR")
